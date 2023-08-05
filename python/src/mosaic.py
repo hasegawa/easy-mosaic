c4a12953fc98420f75a6b8223e0d4d28017124ad
hasegawa <50223611+hasegawa@users.noreply.github.com>
2023-08-05 18:47:27 +0900

    Base64変換メソッドの実装

--- python/src/mosaic.py
+++ python/src/mosaic.py
@@ -1,3 +1,4 @@
+import base64
 import cv2
 import numpy
 
@@ -12,3 +13,12 @@ def mosaic_image(origin_image: numpy.ndarray, x:int, y:int, width:int, height:in
     roi = scalling(scalling(roi, 0.1),10) # 0.1倍縮小程度がちょうどよさそう
     image[y:y + height, x:x + width] = roi
     return image
+
+# Base64からndarrayへ変換する関数
+def base64_to_ndarray(base64_str:str) -> numpy.ndarray:
+    return cv2.imdecode(numpy.fromstring(base64.b64decode(base64_str), numpy.uint8), cv2.IMREAD_UNCHANGED)
+
+# ndarrayからBase64へ変換する関数
+def ndarray_to_base64(image:numpy.ndarray) -> str:
+    (result, encoded_image) = cv2.imencode(".png", image)
+    return base64.b64encode(encoded_image)
